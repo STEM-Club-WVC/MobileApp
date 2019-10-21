7e23aa7efc26e497b156ed1fcf16a4ebc7f7e9f9
Aaron Brunckhurst <dbrunckh@mywvc.wvc.edu>
2019-10-21 12:44:15 -0700

    Inital Commit.

---
+++ WVCSTEMMobleApp/WVCSTEMMobleApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WVCSTEMMobleApp.Models;
+
+namespace WVCSTEMMobleApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
