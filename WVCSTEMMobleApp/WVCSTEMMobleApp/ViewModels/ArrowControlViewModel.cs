84a57ee36f4df2b8dc1b248bb96d6894db6747d5
Aaron Brunckhurst <dbrunckh@mywvc.wvc.edu>
2019-11-20 21:24:06 -0800

    Started adding an arrow control panel.

---
+++ WVCSTEMMobleApp/WVCSTEMMobleApp/ViewModels/ArrowControlViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WVCSTEMMobleApp.ViewModels
+{
+    public class ArrowControlViewModel : BaseViewModel
+    {
+    }
+}
