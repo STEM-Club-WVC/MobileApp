3540659356c610dd0f52a598432373f27d2c3894
Aaron Brunckhurst <dbrunckh@mywvc.wvc.edu>
2019-11-22 17:04:29 -0800

    Started adding Arrow control view model

--- WVCSTEMMobleApp/WVCSTEMMobleApp/ViewModels/ArrowControlViewModel.cs
+++ WVCSTEMMobleApp/WVCSTEMMobleApp/ViewModels/ArrowControlViewModel.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace WVCSTEMMobleApp.ViewModels
 {
     public class ArrowControlViewModel : BaseViewModel
     {
+        public ArrowControlViewModel()
+        {
+            Title = "Arrow Control";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
     }
 }
