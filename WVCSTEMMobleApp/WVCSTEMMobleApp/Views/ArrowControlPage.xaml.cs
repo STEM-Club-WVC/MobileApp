2fb1077ffed46ae420f05eb2c1266c1577a2e46f
Aaron Brunckhurst <dbrunckh@mywvc.wvc.edu>
2019-11-22 18:32:15 -0800

    Fixed a build error and added arrow control page.

---
+++ WVCSTEMMobleApp/WVCSTEMMobleApp/Views/ArrowControlPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WVCSTEMMobleApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ArrowControlPage : ContentPage
+    {
+        public ArrowControlPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
