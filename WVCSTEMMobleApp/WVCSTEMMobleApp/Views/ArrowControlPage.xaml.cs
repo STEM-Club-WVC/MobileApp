a047a219b06ba367fb1ba9bbc386ee0ba2459747
Aaron Brunckhurst <dbrunckh@mywvc.wvc.edu>
2019-12-05 12:52:32 -0800

    Worked on the Arrow control page.

--- WVCSTEMMobleApp/WVCSTEMMobleApp/Views/ArrowControlPage.xaml.cs
+++ WVCSTEMMobleApp/WVCSTEMMobleApp/Views/ArrowControlPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace WVCSTEMMobleApp.Views
@@ -15,6 +16,40 @@ namespace WVCSTEMMobleApp.Views
         public ArrowControlPage()
         {
             InitializeComponent();
+
+            Log("Startup");
+
+            new Button()
+            {
+                Text = "Skills",
+                TextColor = Color.FromRgb(161, 161, 161),
+                BorderColor = Color.FromRgb(161, 161, 161),
+                BorderWidth = 1,
+                HeightRequest = 40,
+                //BorderRadius = 1,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+            };
+
+            Log("Pre-Generate");
+            GenerateControls();
+            Log("Post-Generate");
+        }
+
+        public void GenerateControls()
+        {
+            var center = new Image { Source = "Icon.png", IsVisible = true };
+            var arrowLeft = new Image { Source = "double_arrow.png", IsVisible = true };
+            var arrowRight = new Image { Source = "double_arrow.png", IsVisible = true };
+            arrowRight.RotationY = 180;
+            var arrowTop = new Image { Source = "arrow.jpg", IsVisible = true };
+            arrowTop.RotationY = 90;
+            var arrowBottom = new Image { Source = "arrow.jpg", IsVisible = true };
+            arrowBottom.RotationY = 270;
+        }
+
+        public static void Log(string text)
+        {
+            System.Console.WriteLine(text);
         }
     }
 }
